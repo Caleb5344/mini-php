dcacb7da33a2a08c48f8982a55bce13d1d5cc276
caleb5344 <itscalebodongo@gmail.com.>
2024-03-29 18:27:34 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>PHP clock</title>
+</head>
+
+<body>
+    <h1>php clock</h1>
+    <p><?php
+    echo date("h:i:s A");
+    ?></p>
+</body>
+
+</html>
